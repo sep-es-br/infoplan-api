6ffeac059c70461e0a95f31c67c64c05f1131531
Ronicley Angelo <ronicley8@hotmail.com>
2026-06-17 14:07:07 -0300

    Fix: Adicionado um novo atributo para pega o timesTamp do indicator e quantidade de entregas no painel de Obras

--- src/main/java/br/gov/es/infoplan/dto/IndicatorExecution/response/CardSuccessResponseDTO.java
+++ src/main/java/br/gov/es/infoplan/dto/IndicatorExecution/response/CardSuccessResponseDTO.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 
 public record CardSuccessResponseDTO(
         @JsonProperty("sucesso")
-        BigDecimal success
+        BigDecimal success,
+        String timesTamp
 ) {
 }
